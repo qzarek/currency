71cc08304ab841b04ea58211a68773d19dafc848
qzarek <qzarek@gmail.com>
2021-11-20 18:02:57 +0200

    flake8 and travis ci

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+# from django.http.response import HttpResponse
+# from django.shortcuts import render
 
-# Create your views here.
+
+def rate_list(request):
+    return None
